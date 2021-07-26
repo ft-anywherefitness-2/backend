90a2b2397fe87c47dbd2531ed9e074a90ea46596
bryan <bryan.guner@gmail.com>
2021-07-26 11:45:16 -0400

    update

--- api/server.js
+++ api/server.js
@@ -1,35 +1,24 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const db = require("./data/db-config");
+const authenticate = require("../auth/authenticate-middleware");
 
-function getAllUsers() {
-  return db("users");
-}
-
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-  const [newUserObject] = await db("users").insert(user, [
-    "user_id",
-    "username",
-    "password",
-  ]);
-  return newUserObject; // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+const authRouter = require("../auth/auth-router");
+const usersRouter = require("../users/users-router");
+const classesRouter = require("../classes/classes-router");
 
 const server = express();
-server.use(express.json());
+
 server.use(helmet());
+server.use(express.json());
 server.use(cors());
 
-server.get("/api/users", async (req, res) => {
-  res.json(await getAllUsers());
-});
+server.use("/api/auth", authRouter);
+server.use("/api/users", authenticate, usersRouter);
+server.use("/api/classes", classesRouter);
 
-server.post("/api/users", async (req, res) => {
-  res.status(201).json(await insertUser(req.body));
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Anywhere Fitness API 💪" });
 });
 
 module.exports = server;
