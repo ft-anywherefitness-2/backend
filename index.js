3fcaed6b8792a166056a63ba6baa272dedd10741
Teresa Franxman <teresafranxman97@gmail.com>
2021-07-26 13:26:02 -0500

    Installed additional dependencies, updated port variable in index.js file

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const server = require('./api/server')
 
-const port = process.env.PORT
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-  console.log('listening on ' + port)
+  console.log(`Server listening on localhost:${port}`)
 })
