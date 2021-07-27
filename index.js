0dd8e9345b19da3f79697745e6bb09c881b1421b
Teresa Franxman <teresafranxman97@gmail.com>
2021-07-27 13:05:28 -0500

    Updated server.listen

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = require('./api/server')
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-  console.log(`Server listening on localhost:${port}`)
+  console.log(`Running server on port:${port}`)
 })
