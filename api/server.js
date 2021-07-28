32acf8597f1b65a86c280930b9f83bc104ccf8f2
Teresa Franxman <teresafranxman97@gmail.com>
2021-07-27 23:59:14 -0500

    Created error handling middleware

--- api/server.js
+++ api/server.js
@@ -18,4 +18,11 @@ server.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the Anywhere Fitness API 💪" });
 });
 
+server.use((err, req, res, next) => { //eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+  })
+});
+
 module.exports = server
