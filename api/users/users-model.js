593d26e7898c67b8fe87207272d11b2048e125e8
Teresa Franxman <teresafranxman97@gmail.com>
2021-07-28 20:24:25 -0500

    can now get user with role included

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,14 +1,15 @@
 const db = require('../../data/db-config');
 
 function getAllUsers() {
-    return db('users')
+    return db('users as u')
+        .leftJoin('roles', 'u.role_id', 'roles.role_id')
 }
 
 async function insertUser(user) {
     // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
     // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
     // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-    const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
+    const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password', 'role_id'])
     return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
 }
 
