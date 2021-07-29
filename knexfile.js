cd4222dc4597f51ffadc79174308bbd53d68ba73
bryan <bryan.guner@gmail.com>
2021-07-29 12:26:11 -0400

    migrations

--- knexfile.js
+++ knexfile.js
@@ -1,45 +1,44 @@
+// Update with your config settings.
+
 module.exports = {
+
   development: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: "./database/anywhere-fitness.db3",
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
+      filename: './dev.sqlite3'
+    }
   },
-  // needed when using foreign keys
-  pool: {
-    afterCreate: (conn, done) => {
-      // runs after a connection is made to the sqlite engine
-      conn.run("PRAGMA foreign_keys = ON", done); // turn on foreign key enforcement
-    },
-  },
-  testing: {
-    client: "sqlite3",
+
+  staging: {
+    client: 'postgresql',
     connection: {
-      filename: "./database/test.db3",
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
-    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
+      tableName: 'knex_migrations'
+    }
   },
+
   production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
     migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-  },
+      tableName: 'knex_migrations'
+    }
+  }
+
 };
